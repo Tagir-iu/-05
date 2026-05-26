791770db44fc4bc953fe28efea1938bbb3aeb1df
Tagir-iu <taga07taga@gmail.com>
2026-05-26 08:18:14 +0300

    Delete solver.h

--- solver.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <complex>
-#include <utility>
-
-using solution = std::pair<std::complex<double>, std::complex<double>>;
-solution solve_quadratic(double a, double b, double c);
