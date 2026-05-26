dbdd906f66d7b4cb6edb521476411629524fef97
Tagir-iu <taga07taga@gmail.com>
2026-05-26 08:18:59 +0300

    Delete formatter.cpp

--- formatter.cpp
+++
@@ -1,9 +0,0 @@
-#include "formatter.h"
-
-std::string formatter(const std::string& message) {
-    return "[" + message + "]";
-}
-
-void formatter(std::ostream& os, const std::string& message) {
-    os << "[" << message << "]";
-}
